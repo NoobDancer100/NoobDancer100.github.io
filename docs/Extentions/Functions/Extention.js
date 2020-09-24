70e33f821d35b09f4eff65a75656fc80e857e1a9
NoobDancer100 <71303544+NoobDancer100@users.noreply.github.com>
2020-09-24 17:50:21 -0600

    Create Extention.js

---
+++ docs/Extentions/Functions/Extention.js
@@ -0,0 +1,35 @@
+(function(ext) {
+    var run = "";
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+    
+    ext.run = function(name) {
+      run = name;
+    };
+    
+    ext.hat = function(name) {
+      if (run === name) {
+        run = "";
+        return true;
+      }
+      
+      return false;
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+        [" ", "run function %s", "run"],
+        ["h", "function %s", "hat"],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Sample extension', descriptor, ext);
+})({});
