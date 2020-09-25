7a00a6deead5cc1eff8d219c4b1749e8fa265eef
NoobDancer100 <71303544+NoobDancer100@users.noreply.github.com>
2020-09-25 10:02:32 -0600

    Create Extention.js

---
+++ docs/Extentions/NewExtention/Extention.js
@@ -0,0 +1,32 @@
+(function(ext) {
+    var open = false;
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+    
+    ext.waitopened = function(callback) {
+      while(open==false) {}
+      open = false
+      callback();
+    };
+    
+    ext.openwait = function() {
+      open = true
+    }
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+          ["w", "wait until opened", "waitopened"]
+          [" ", "open wait", "openwait"]
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Sample extension', descriptor, ext);
+})({});`
